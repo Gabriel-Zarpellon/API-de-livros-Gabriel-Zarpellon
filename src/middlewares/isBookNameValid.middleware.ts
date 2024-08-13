ec2059ecfdbdf793f5dae5957a6fe1691e79a4a8
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-08-13 15:31:17 -0300

    fix: add option for req.body.name to be undefined

--- src/middlewares/isBookNameValid.middleware.ts
+++ src/middlewares/isBookNameValid.middleware.ts
@@ -4,7 +4,7 @@ import { AppError } from "../errors/appError";
 
 export class IsBookNameValid {
   static execute(req: Request, res: Response, next: NextFunction) {
-    if (booksDatabase.some((book) => book.name.toLowerCase() == req.body.name.toLowerCase())
+    if (booksDatabase.some((book) => book.name.toLowerCase() == req.body.name?.toLowerCase())
     ) {
       throw new AppError(409, "Book already registered.");
     }
